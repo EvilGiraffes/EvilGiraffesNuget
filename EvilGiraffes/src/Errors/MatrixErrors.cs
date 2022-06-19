ed71bf06e099b15843cb952402a90d4ae44c061e
EvilGiraffes <85831955+EvilGiraffes@users.noreply.github.com>
2022-06-19 14:34:55 +0200

    Refactor

--- EvilGiraffes/src/Errors/MatrixErrors.cs
+++ EvilGiraffes/src/Errors/MatrixErrors.cs
@@ -1,8 +1,22 @@
 namespace EvilGiraffes.Errors;
+/// <summary>
+/// Will be thrown if the matrix has already been initialized.
+/// </summary>
 public class MatrixInitializedException: BaseException {}
+/// <summary>
+/// Will be thrown if setting out of bounds of the matrix.
+/// </summary>
 public class MatrixOutOfBoundsException: BaseException
 {
+    /// <summary>
+    /// The length of matrix row or column
+    /// </summary>
+    /// <value>Int value of the length</value>
     public int MatrixLength { get; init; }
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <value></value>
     public int FullLength { get; init; }
     public MatrixOutOfBoundsException(int matrixLength, int fullLength)
     {
